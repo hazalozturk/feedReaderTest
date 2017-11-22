1c5942076a14aea5c15654dd06488a0aeabd2d5f
Hazal Ozturk <hcanisag@gmail.com>
2017-11-22 16:38:09 -0500

    Write a new The Menu suite to test menu changes on click event

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -40,18 +40,32 @@ $(function() {
 
 
     /* TODO: Write a new test suite named "The menu" */
+    describe('The menu', function() {
 
-        /* TODO: Write a test that ensures the menu element is
-         * hidden by default. You'll have to analyze the HTML and
-         * the CSS to determine how we're performing the
-         * hiding/showing of the menu element.
-         */
+      /* TODO: Write a test that ensures the menu element is
+       * hidden by default. You'll have to analyze the HTML and
+       * the CSS to determine how we're performing the
+       * hiding/showing of the menu element.
+       */
+       it('has hidden menu element by default', function() {
+         expect(document.body.className).toContain('menu-hidden');
+       });
 
-         /* TODO: Write a test that ensures the menu changes
-          * visibility when the menu icon is clicked. This test
-          * should have two expectations: does the menu display when
-          * clicked and does it hide when clicked again.
-          */
+       /* TODO: Write a test that ensures the menu changes
+        * visibility when the menu icon is clicked. This test
+        * should have two expectations: does the menu display when
+        * clicked and does it hide when clicked again.
+        */
+        it('changes the menu when clicked', function() {
+          document.querySelector('.menu-icon-link').click();
+          expect(document.body.className).not.toContain('menu-hidden');
+
+          document.querySelector('.menu-icon-link').click();
+          expect(document.body.className).toContain('menu-hidden');
+        });
+    })
+
+
 
     /* TODO: Write a new test suite named "Initial Entries" */
 
