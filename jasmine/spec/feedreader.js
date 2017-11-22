2dae8d0fcbd8523e0ae5ab59130dd5f8c60afc2f
Hazal Ozturk <hcanisag@gmail.com>
2017-11-22 16:35:24 -0500

    Write a test to ensure each feed in allFeeds array has a name defined

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -29,6 +29,13 @@ $(function() {
          * in the allFeeds object and ensures it has a name defined
          * and that the name is not empty.
          */
+
+         it('has a name defined', function() {
+           allFeeds.forEach(function(feed) {
+             expect(feed.name).toBeDefined();
+             expect(feed.name.length).not.toBe(0);
+           });
+         });
     });
 
 
